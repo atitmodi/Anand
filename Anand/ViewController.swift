2b7f258557714cb32898f7d9efad30bca40ef3e5
Atit Modi <atitmodi2011@gmail.com>
2018-04-19 12:46:36 +0530

    first commit

--- Anand/ViewController.swift
+++ Anand/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("did load")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
